c7838f8f812345f10aacd5667866c111c5091b69
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-02-22 16:08:51 +0200

    Update index.js

--- osa3/index.js
+++ osa3/index.js
@@ -104,6 +104,7 @@ let generateId = ()=>{
 }
 
 
-const PORT = process.env.PORT || 3001app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
