f5d2db93cfa257209a86e5d9ac65ad9fb46bc800
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-04-14 11:36:53 +0300

    anedcotes custom hooks done

--- osa7/src/App.js
+++ osa7/src/App.js
@@ -8,6 +8,8 @@ import {
   useHistory,
 } from "react-router-dom"
 
+import  { useField } from './hooks'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5,
@@ -96,20 +98,30 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const history = useHistory()
-  const [content, setContent] = useState("")
-  const [author, setAuthor] = useState("")
-  const [info, setInfo] = useState("")
+
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('url')
+
+
+  const onReset = () => {
+    content.onClick()
+    author.onClick()
+    info.onClick()
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content : content.value,
+      author : author.value,
+      info : info.value,
       votes: 0,
     })
     history.push("/")
   }
+
 
   return (
     <div>
@@ -117,35 +129,25 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content}/>
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author}/>
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info}/>
         </div>
-        <button>create</button>
+        <button>create</button>   
       </form>
+      <button onClick={onReset}>reset</button>
     </div>
   )
 }
 
 const App = () => {
+  
   const [anecdotes, setAnecdotes] = useState([
     {
       content: "If it hurts, do it more often",
