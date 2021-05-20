4916d744415a6d546f9e1d70969a07215b4766e8
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-05-20 07:16:26 +0300

    9.2 Done

--- osa9TS/bmiCalc/exerciseCalculator.ts
+++ osa9TS/bmiCalc/exerciseCalculator.ts
@@ -1,28 +1,56 @@
-
-type Result = number | string
-
-
-interface MultiplyValues {
-    h: number;
-    w: number;
+interface Result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
 }
 
-const parseArguments = (args: Array<string>): MultiplyValues => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
+const calculateExercises = (arr: Array<number>, target: number): Result => {
 
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-        return {
-            h: Number(args[2]),
-            w: Number(args[3])
-        }
-    } else {
-        throw new Error('Provided values were not numbers!');
+    let avg = arr.reduce((a, b) => a + b) / arr.length
+    console.log(avg)
+
+    const metric = (target - avg)
+    console.log(metric)
+    let rating: number
+    if (metric >= 5) { rating = 1 }
+    else if (metric <= 0) { rating = 3 }
+    else { rating = 2 }
+
+    let desc : string;
+    switch (metric) {
+        case 1:
+            desc = "Terrible job, im dissapointed";
+            break;
+        case 2:
+            desc = "not too bad, but next week we can do better!"
+        case 3:
+            desc = "you are the ultimate fitness god"
+        default:
+            desc = "what is dis"
+            break;
     }
+
+
+    let result : Result = {
+        "periodLength": arr.length,
+        "trainingDays": arr.filter(v => v > 0).length,
+        "success": (avg >= target),
+        "rating": rating,
+        "ratingDescription": desc,
+        "target" : target,
+        "average": avg,
+    }
+
+    return result
+    
 }
 
 try {
-    const { h, w } = parseArguments(process.argv);
+    calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2)
     //TODO(h, w);
 } catch (e) {
     console.log('Error, something bad happened, message: ', e.message);
