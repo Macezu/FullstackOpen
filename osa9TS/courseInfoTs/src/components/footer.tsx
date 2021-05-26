e41d5c80100f0bb5fb51b35a32bda8dbaedfb583
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-05-26 07:16:00 +0300

    9.14 Footer remains

---
+++ osa9TS/courseInfoTs/src/components/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+interface Parts{
+    exerciseCount : number
+}
+
+const Footer = ({courseParts} : Array<Parts>) => {
+  return (
+    <div>
+      <p>
+        Number of exercises
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  )
+}
+
+export default Footer
