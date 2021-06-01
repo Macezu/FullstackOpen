ab83f6aab7ce60e2d757afd38c255acc02cf2626
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-01 07:26:12 +0300

    backend revamped with entries

--- osa9TS/patientor/patientorback/types/PatientEntry.ts
+++ osa9TS/patientor/patientorback/types/PatientEntry.ts
@@ -1,25 +1,73 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry{
+import { DiagnoseEntry } from "./DiagnoseEntry";
 
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
 }
 
-export interface PatientEntry{
-    "id": string;
-    "name": string;
-    "dateOfBirth": string;
-    "ssn"?: string;
-    "gender": Gender;
-    "occupation": string;
-    "entries"? : Entry[];
-}
-
-export enum Gender{
+export enum Gender {
     Male = "male",
     Female = "female",
     Other = "other"
 }
 
 
+export interface BaseEntry {
+    id: string;
+    date: string;
+    description: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseEntry["code"]>
+}
+
+export interface PatientEntry {
+    "id": string;
+    "name": string;
+    "dateOfBirth": string;
+    "ssn"?: string;
+    "gender": Gender;
+    "occupation": string;
+    "entries"?: Array<Entry>;
+};
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+};
+
+interface Discharge {
+    date: string;
+    criteria: string;
+};
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+};
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+};
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+};
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry
+
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export type NSPatientEntry = Omit<PatientEntry, 'ssn'>;
 
