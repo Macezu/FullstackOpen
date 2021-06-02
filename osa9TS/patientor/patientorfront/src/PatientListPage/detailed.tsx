3c300ce8a9f5a1c123f921bd7eb0dffeeaf57011
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-02 07:16:02 +0300

    update

--- osa9TS/patientor/patientorfront/src/PatientListPage/detailed.tsx
+++ osa9TS/patientor/patientorfront/src/PatientListPage/detailed.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Icon } from "semantic-ui-react";
 import { useStateValue } from "../state";
-import { Patient } from "../types";
+import {  Patient, Diagnosis } from "../types";
+
+const diagnoseCodes = ({code} : { code : string}) : React.JSXElementConstructor =>{
+  return <li>code</li>
+}
 
 const DetailedPatient = () => {
   const [{ patients } ] = useStateValue();
   const { id } = useParams<{ id: string }>();
   const patient = Object.values(patients).find((p : Patient) : boolean => p.id === id);
-  
-
+  const entries  = patient?.entries[0];
+  console.log(entries);
   return (
     <div>
       <h2>{patient?.name} {patient?.gender === "male" ? (
@@ -23,7 +27,10 @@ const DetailedPatient = () => {
       <p>ssn: {patient?.ssn}</p>
       <p>occupation: {patient?.occupation}</p>
       <h5>entries</h5>
-      
+      <p>{entries?.date} {entries?.description}</p>
+      <ul>
+        {entries?.diagnosisCodes?.forEach(x => diagnoseCodes(x))}
+      </ul>
     </div>
   );
 };
