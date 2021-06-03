f526db30c147deffd54987f4b87c5f3ef3e876c6
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-03 18:34:34 +0300

    Entries almost added

--- osa9TS/patientor/patientorfront/src/PatientListPage/detailed.tsx
+++ osa9TS/patientor/patientorfront/src/PatientListPage/detailed.tsx
@@ -5,44 +5,60 @@ import { useParams } from "react-router-dom";
 import { Icon } from "semantic-ui-react";
 import { apiBaseUrl } from "../constants";
 import { useStateValue } from "../state";
-import { Diagnosis, Patient } from "../types";
+import { Diagnosis, Patient, Entry } from "../types";
+
+
+
+
+
 
 const DetailedPatient = () => {
   const [{ patients }] = useStateValue();
   const { id } = useParams<{ id: string }>();
-  const patient = Object.values(patients).find((p: Patient): boolean => p.id === id);
-  const entries = patient?.entries[0];
+  const patient = Object.values(patients).find(
+    (p: Patient): boolean => p.id === id
+  );
+  const entries = patient?.entries;
   const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
 
-  const GetDiagnoses = ({ patientEntries } : { patientEntries : string[] | undefined} ) : ReactElement => {
-
-    if (patientEntries !== undefined && diagnoses !== undefined){
-      const found = diagnoses.filter(x => patientEntries.includes(x.code));
-      const listed = found.map(x => <li key={x.name}>{x.code} {x.name} {x.latin}</li>);
-      console.log(listed);
-      return <ul>{listed}</ul>;
+  const EntryMapped = ({entries} : {entries : Array<Entry> | undefined}) :ReactElement => {
+    if (entries) {
+      
+      const mapped = entries.forEach(entry => {
+        switch (entry.type) {
+          case "HealthCheck":
+            return entry.healthCheckRating;
+          case "Hospital":
+            return entry.specialist;
+          case "OccupationalHealthcare":
+            return entry.employerName;
+          default:
+            return assertNever(entry);
+        }
+      });
+  
+      console.log(mapped);
+      return <ul></ul>;  
     }
-    
-    return <ul>No Entries</ul>;
+    return <ul></ul>;
   };
+  
 
   React.useEffect(() => {
-  const fetchDiagnoseList = async () => {
-    
-    try {
-      const { data: diagnoseListFromApi }  = await axios.get<Diagnosis[]>(
-        `${apiBaseUrl}/diagnoses`
-      );
-      setDiagnoses(diagnoseListFromApi);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  void fetchDiagnoseList();
-  },[]);
+    const fetchDiagnoseList = async () => {
+      try {
+        const { data: diagnoseListFromApi } = await axios.get<Diagnosis[]>(
+          `${apiBaseUrl}/diagnoses`
+        );
+        setDiagnoses(diagnoseListFromApi);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    void fetchDiagnoseList();
+  }, []);
 
-  console.log(diagnoses);
   return (
     <div>
       <h2>
@@ -59,8 +75,14 @@ const DetailedPatient = () => {
       <p>ssn: {patient?.ssn}</p>
       <p>occupation: {patient?.occupation}</p>
       <h5>entries</h5>
-      <GetDiagnoses patientEntries={entries?.diagnosisCodes} />
+      <EntryMapped entries={entries} />
     </div>
+  );
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
   );
 };
 
