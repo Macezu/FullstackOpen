7f1a0683e0a072fee8a943a55457811b89a251b5
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-07 07:17:11 +0300

    typeguard update

--- osa9TS/patientor/patientorback/utilities/handleAddEntry.ts
+++ osa9TS/patientor/patientorback/utilities/handleAddEntry.ts
@@ -1,10 +1,26 @@
 import { NewEntryEntry } from "../types/PatientEntry"
+import { DiagnoseEntry } from "../types/DiagnoseEntry"
 
 type Fields = { date: unknown, description: unknown, specialist: unknown, diagnosisCodes: unknown, type: unknown, healthCheckRating : unknown, employerName : unknown, sickLeave : unknown, discharge : unknown  };
 
 const isString = (text: unknown): text is string => {
     return typeof text === 'string' || text instanceof String;
 };
+
+const isArray = (arr : unknown): arr is Array<string> =>{
+    return arr instanceof Array
+}
+
+const isNumber = (num : unknown): num is number => {
+    return typeof num === "number" || num instanceof Number
+}
+
+const isStringArray = (value : unknown) :string[] => {
+    if (!value ||!isArray(value) || isString(value[0])){
+        throw new Error('Incorrect or missing codes');
+    }
+    return value;
+}
 
 
 const parseString = (name: unknown): string => {
@@ -14,13 +30,21 @@ const parseString = (name: unknown): string => {
     return name;
 }
 
+const parseNumber = (numb : unknown) : number =>{
+    if (!numb || isNumber(numb)) {
+        throw new Error("Incorrect or mising HealthCheck")
+    }
+    return numb
+}
 
-const handleAddEntry = ({ date, description, specialist, diagnosisCodes, type, healthCheckRating, employerName, sickLeave?, discharge } : Fields) : NewEntryEntry => {
+
+const handleAddEntry = ({ date, description, specialist, diagnosisCodes, type, healthCheckRating, employerName, sickLeave, discharge } : Fields) : NewEntryEntry => {
     const entry: NewEntryEntry = {
         date: parseString(date),
         description : parseString(description),
         specialist : parseString(specialist),
-        diagnosisCodes : parseString(diagnosisCodes)
+        diagnosisCodes : isStringArray(diagnosisCodes),
+        healthCheckRating : parseNumber(healthCheckRating)
 
     }
     switch (type) {
