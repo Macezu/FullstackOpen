ead6a71ff4cedac0b7985a97bb29bd6e6abff194
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-17 08:54:37 +0300

    Introduction to Native components

--- osa10ReactNative/rate-repository-app/App.js
+++ osa10ReactNative/rate-repository-app/App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const HelloWorld = ({name})  => {
+  return <Text>Hello {name}</Text>;};
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>TESTIN</Text>
+      <HelloWorld name="Kalle"/>
       <StatusBar style="auto" />
     </View>
   );
