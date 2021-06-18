b6a1ab42608c60a325e327429bfa20295bb7a6ee
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-18 20:01:31 +0300

    10.5 Halfay

--- osa10ReactNative/rate-repository-app/src/components/RepositoryItem.jsx
+++ osa10ReactNative/rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,18 +1,35 @@
 import React from "react";
-import { View } from "react-native";
-import Text from './Text';
+import { View, Image, StyleSheet } from "react-native";
+import theme from "../theme";
+import Text from "./Text";
+
+const styles = StyleSheet.create({
+  containerRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  containerCol: {
+    flexDirection: "column"
+  },
+  tinyLogo: theme.images.thumbnail,
+  medium: theme.images.medium
+});
 
 const RepositoryItem = ({ item }) => {
   return (
     <View>
-      <Text fontWeight="bold">Name : {item.fullName}</Text>
+      <View style={styles.containerRow}>
+        <Image style={styles.tinyLogo} source={item.ownerAvatarUrl} />
+        <Text fontWeight="bold">Name : {item.fullName}</Text>
+      </View>
       <Text>description : {item.description}</Text>
       <Text>language : {item.language}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Ratings: {item.ratingAverage}</Text>
-      <Text>Rewviews: {item.reviewCount}</Text>
-      <Text>Avatar Url: {item.ownerAvatarUrl}</Text>
+      <View style={styles.containerRow}>
+        <Text>Forks: {item.forksCount}</Text>
+        <Text>Stars: {item.stargazersCount}</Text>
+        <Text>Ratings: {item.ratingAverage}</Text>
+        <Text>Rewviews: {item.reviewCount}</Text>
+      </View>
     </View>
   );
 };
--- osa10ReactNative/rate-repository-app/src/theme.js
+++ osa10ReactNative/rate-repository-app/src/theme.js
@@ -1,23 +1,37 @@
 const theme = {
-    colors: {
-      textPrimary: '#24292e',
-      textSecondary: '#586069',
-      primary: '#0366d6',
-      appbarBG: '#f39bfc20'
+  colors: {
+    textPrimary: "#24292e",
+    textSecondary: "#586069",
+    primary: "#0366d6",
+    appbarBG: "#f39bfc20"
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    header: 22
+  },
+  fonts: {
+    main: "System"
+  },
+  fontWeights: {
+    normal: "400",
+    bold: "700",
+    xtrabold: "1000"
+  },
+  images: {
+    thumbnail: {
+      width: 50,
+      height: 50
     },
-    fontSizes: {
-      body: 14,
-      subheading: 16,
-      header: 22
+    medium: {
+      width: 100,
+      height: 100
     },
-    fonts: {
-      main: 'System',
-    },
-    fontWeights: {
-      normal: '400',
-      bold: '700',
-      xtrabold: '1000'
-    },
-  };
-  
-  export default theme;
+    big: {
+      width: 150,
+      height: 150
+    }
+  }
+};
+
+export default theme;
