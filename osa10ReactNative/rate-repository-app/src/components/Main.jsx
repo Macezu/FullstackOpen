1c27905026abf863249e26a787a9640466ab8168
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-20 23:14:33 +0300

    10.6 Init

--- osa10ReactNative/rate-repository-app/src/components/Main.jsx
+++ osa10ReactNative/rate-repository-app/src/components/Main.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import RepositoryList from './RepositoryList';
-import { StyleSheet, View } from 'react-native';
-import AppBar from './AppBar';
+import React from "react";
+import RepositoryList from "./RepositoryList";
+import SignIn from "./SignIn";
+import { StyleSheet, View } from "react-native";
+import { Route, Switch, Redirect } from "react-router-native";
+import AppBar from "./AppBar";
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    flexShrink: 1,
-  },
+    flexShrink: 1
+  }
 });
-
-
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <AppBar/>
-      <RepositoryList/>
+      <AppBar />
+      <Switch>
+        <Route path="/signin" exact>
+          <SignIn />
+        </Route>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
   );
 };
