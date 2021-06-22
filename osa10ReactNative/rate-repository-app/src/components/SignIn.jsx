fe5bf83a87d9de6701a5e0406a59a0e4acaac793
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-22 08:48:08 +0300

    first draft

--- osa10ReactNative/rate-repository-app/src/components/SignIn.jsx
+++ osa10ReactNative/rate-repository-app/src/components/SignIn.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import FormikTextInput from './FormikTextInput';
-import { Text,  Pressable, View } from 'react-native';
-import { Formik } from 'formik';
+import React from "react";
+import FormikTextInput from "./FormikTextInput";
+import { Text, Pressable, View } from "react-native";
+import { Formik } from "formik";
 
 const initialValues = {
-  mass: '',
-  height: '',
+  username: "",
+  password: ""
 };
 
-const getBodyMassIndex = (mass, height) => {
-  return Math.round(mass / Math.pow(height, 2));
-};
 
-const onSubmit = (values) => {
+const logSubmit = (values) => {
   console.log(values);
 };
 
-const BodyMassIndexForm = ({ onSubmit }) => {
+const LoginForm = ({ onSubmit }) => {
   return (
     <View>
-      <FormikTextInput name="mass" placeholder="Weight (kg)" />
-      <FormikTextInput secureTextEntry={true} name="height" placeholder="Height (m)" />      
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput
+        secureTextEntry={true}
+        name="password"
+        placeholder="Password"
+      />
       <Pressable onPress={onSubmit}>
         <Text>Calculate</Text>
       </Pressable>
@@ -28,19 +29,20 @@ const BodyMassIndexForm = ({ onSubmit }) => {
   );
 };
 
-const BodyMassIndexCalculator = () => {
-  const onSubmit = values => {
-    const mass = parseFloat(values.mass);
-    const height = parseFloat(values.height);
+const LoginView = () => {
+  const onSubmit = (values) => {
+    const username = toString(values.username);
+    const password = toString(values.password);
 
-    if (!isNaN(mass) && !isNaN(height) && height !== 0) {
-      console.log(`Your body mass index is: ${getBodyMassIndex(mass, height)}`);
+    if (isNaN(username) && isNaN(password) && password !== 0) {
+      logSubmit(username, password);
     }
   };
-
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {({ handleSubmit }) => <BodyMassIndexForm onSubmit={handleSubmit} />}
+      {({ handleSubmit }) => <LoginForm onSubmit={handleSubmit} />}
     </Formik>
   );
 };
+
+export default LoginView;
