6b02e71714c8dd5f5fe5e0a2fe274c6aa2792b9c
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-06-23 08:53:40 +0300

    10.9 50% Done

--- osa10ReactNative/rate-repository-app/src/components/SignIn.jsx
+++ osa10ReactNative/rate-repository-app/src/components/SignIn.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import FormikTextInput from "./FormikTextInput";
-import { StyleSheet, View,Button } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import { Formik } from "formik";
 import theme from "../theme";
+import * as yup from "yup";
 
 const styles = StyleSheet.create({
   containerCol: {
@@ -11,8 +12,7 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 15,
     borderWidth: 2,
-    backgroundColor: theme.colors.secondary,
-    
+    backgroundColor: theme.colors.secondary
   }
 });
 
@@ -21,9 +21,15 @@ const initialValues = {
   password: ""
 };
 
+const validationSchema = yup.object().shape({
+  username: yup
+    .required("Weight is required"),
+  password: yup
+    .required("Height is required")
+});
 
-const logSubmit = (username,password) => {
-  console.log(username , password);
+const logSubmit = (username, password) => {
+  console.log(username, password);
 };
 
 const LoginForm = ({ onSubmit }) => {
@@ -35,20 +41,23 @@ const LoginForm = ({ onSubmit }) => {
         name="password"
         placeholder="Password"
       />
-        <Button onPress={onSubmit} title="Send"></Button> 
+      <Button onPress={onSubmit} title="Send"></Button>
     </View>
   );
 };
 
 const LoginView = () => {
   const onSubmit = (values) => {
-    
     const username = values.username;
     const password = values.password;
-    logSubmit(username,password);
+    logSubmit(username, password);
   };
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {({ handleSubmit }) => <LoginForm onSubmit={handleSubmit} />}
     </Formik>
   );
