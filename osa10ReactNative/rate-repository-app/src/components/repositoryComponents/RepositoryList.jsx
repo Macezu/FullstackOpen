7b408f2d4bec080f428495c809c4487e267ab680
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-06 12:04:41 +0300

    10.24 Done

--- osa10ReactNative/rate-repository-app/src/components/repositoryComponents/RepositoryList.jsx
+++ osa10ReactNative/rate-repository-app/src/components/repositoryComponents/RepositoryList.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
-import useRepositories from "../../hooks/useRepositories"
+import React, { useState } from "react";
+import useRepositories from "../../hooks/useRepositories";
 import RepositoryItem from "./RepositoryItem";
 import Menu from "./Menu";
 import { FlatList, View, StyleSheet } from "react-native";
-
+import { Searchbar } from "react-native-paper";
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   separator: {
@@ -11,42 +12,54 @@ const styles = StyleSheet.create({
   }
 });
 
-
 const ItemSeparator = () => <View style={styles.separator} />;
 
-
-export const RepositoryListContainer = ({ repositories, setOrderBy, orderBy }) => {
+export const RepositoryListContainer = ({
+  repositories,
+  setOrderBy,
+  orderBy,
+  onChangeSearch,
+  searchQuery
+}) => {
   // Get the nodes from the edges array
   const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+    ? repositories.edges.map((edge) => edge.node)
     : [];
-  
+
   const renderItem = ({ item }) => <RepositoryItem item={item} />;
 
-
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem = {renderItem}
-      keyExtractor={(item) => item.id}
-      ListHeaderComponent={() => <Menu  setOrderBy={setOrderBy} orderBy={orderBy}/>}
-    />
+    <>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={() => (
+          <Menu setOrderBy={setOrderBy} orderBy={orderBy} />
+        )}
+      />
+    </>
   );
 };
-
 
 const RepositoryList = () => {
   const [orderBy, setOrderBy] = useState("Latest");
   const [searchQuery, setSearchQuery] = useState("");
   const onChangeSearch = (query) => setSearchQuery(query);
+  const [debounced] = useDebounce(searchQuery, 500);
 
   const orderSelection = (searchKeyword) => {
     const reOrganizeRepos = {
       orderBy: "CREATED_AT",
       orderDirection: "DESC",
       searchKeyword: searchKeyword,
-      first: 6,
+      first: 6
     };
     switch (orderBy) {
       case "Latest":
@@ -63,13 +76,17 @@ const RepositoryList = () => {
     }
   };
 
-  const { repositories} = useRepositories(
-    orderSelection(searchQuery)
-  );
+  const { repositories } = useRepositories(orderSelection(debounced));
 
-  
-  return <RepositoryListContainer repositories={repositories} setOrderBy={setOrderBy} orderBy={orderBy} />;
-  
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      setOrderBy={setOrderBy}
+      orderBy={orderBy}
+      onChangeSearch={onChangeSearch}
+      searchQuery={searchQuery}
+    />
+  );
 };
 
 export default RepositoryList;
